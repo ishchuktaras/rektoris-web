a4e96546b55b61240c94ac3c3992d7fa98579df9
Taras Ishchuk <ishchuktaras@gmail.com>
2024-12-14 15:53:58 +0100

    Non usable import delete

--- components/frontend/hero-section.tsx
+++ components/frontend/hero-section.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { ArrowRight, GraduationCap } from 'lucide-react'
+import { ArrowRight } from 'lucide-react'
 import SectionHeader from "./section-header"
 
 
