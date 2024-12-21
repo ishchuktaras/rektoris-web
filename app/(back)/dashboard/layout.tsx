a47c9cbb8a66740f51c98d823b8c1eefb70c2b99
Taras Ishchuk <ishchuktaras@gmail.com>
2024-12-21 17:45:16 +0100

    Dashboard page add

--- app/(back)/dashboard/layout.tsx
+++ app/(back)/dashboard/layout.tsx
@@ -1,19 +1,7 @@
-import AppSidebar from '@/components/dashboard/sidebar/app-sidebar'
-import SidebarHeader from '@/components/dashboard/sidebar/sidebar-header'
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import React, { ReactNode } from 'react'
 
 export default function DashboardLayout({children}:{children:ReactNode}) {
   return (
-    <div>
-      <SidebarProvider>
-      <AppSidebar/>
-      <SidebarInset>
-        {/* Sidebar header */}
-        <SidebarHeader />
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
-    </div>
+    <div>{children}</div>
   )
 }
--- app/(back)/dashboard/students/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>
-      <h2>Students</h2>
-    </div>
-  )
-}
