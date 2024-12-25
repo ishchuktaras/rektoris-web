04d9e7591a30b037aad109e99e4015cc5ec95d26
Taras Ishchuk <ishchuktaras@gmail.com>
2024-12-25 19:52:49 +0100

    Rename sidebar file

--- app/(back)/dashboard/layout.tsx
+++ app/(back)/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import AppSidebar from '@/components/dashboard/sidebar/sidebar'
+import AppSidebar from '@/components/dashboard/sidebar/app-sidebar'
 import SidebarHeader from '@/components/dashboard/sidebar/sidebar-header'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import React, { ReactNode } from 'react'
