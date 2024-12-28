74fabfa3ff8f111606d861c21384fe479f6ab2e8
Taras Ishchuk <ishchuktaras@gmail.com>
2024-12-28 10:58:06 +0100

    Favicon edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
+  subsets: ["latin"],
+  display: "swap",
+});
 import "./globals.css";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "react-hot-toast";
 
-
 export const metadata: Metadata = {
   title: "Škola Pro - Komplexní řešení pro správu škol",
-  description: "Zefektivněte provoz vaší vzdělávací instituce s naším all-in-one softwarem pro správu škol. Navrženo pro zvýšení efektivity a zlepšení komunikace mezi administrátory, učiteli, studenty a rodiči",
+  description:
+    "Zefektivněte provoz vaší vzdělávací instituce s naším all-in-one softwarem pro správu škol. Navrženo pro zvýšení efektivity a zlepšení komunikace mezi administrátory, učiteli, studenty a rodiči",
 };
 
 export default function RootLayout({
@@ -25,17 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}
-        className={inter.className}
-      >
-        <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-        <NextSSRPlugin
-          
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
+      <body suppressHydrationWarning={true} className={inter.className}>
+        <meta name="apple-mobile-web-app-title" content="Škola Pro" />
+        <Toaster position="top-center" reverseOrder={false} />
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         {children}
         <SpeedInsights />
       </body>
